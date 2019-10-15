76a47e1f7d97894571e495186c2351e98afcc3a7
albert.hodur <albert.hodur@assentis.com>
2019-10-15 08:25:38 +0200

    message change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ohh yeee Albert!!! Works!! But not forget to stop... AWS
+          Ohh yeee Albert!!! Works!! But not forget to stop... AWS!!!
         </p>
         <a
           className="App-link"
